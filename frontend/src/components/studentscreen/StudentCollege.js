0de285df509a58772ffd8d1018fa802cef2f3088
unknown <joseph.mignone@stonybrook.edu>
2020-03-08 21:31:39 -0400

    college cards seem to be finished. Filters could lokk better but will do

--- frontend/src/components/studentscreen/StudentCollege.js
+++ frontend/src/components/studentscreen/StudentCollege.js
@@ -7,11 +7,24 @@ import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from "materialize-css";
 
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css";
+
 class StudentCollege extends React.Component {
 
     state  = 
     {
         status: this.props.college.status
+    }
+
+    getName= () => {
+        if(this.props.college.name.length>32)
+        {
+            let tempName = this.props.college.name.substring(0,30) + "..."
+            return tempName
+        }
+        else
+            return this.props.college.name
     }
 
     getStatus = () => {
@@ -20,6 +33,36 @@ class StudentCollege extends React.Component {
 
     changeStatus  = (newStatus) => {
         this.setState({status: newStatus})
+    }
+
+    getPercent = (type, amount) => {
+        if(type=="math")
+        {
+            return (amount/800 * 100)
+        }
+        if(type=="ebrw")
+        {
+            return (amount/800 * 100)
+        }
+        if(type=="act")
+        {
+            return (amount/36 * 100)
+        }
+        if(type=="rec")
+        {
+            return (amount)
+        }
+    }
+
+    getRecColor = (score) => {
+        if(score<25)
+            return 'rgb(252, 3, 3)'
+        else if(score<50)
+            return 'rgb(252, 207, 3)'
+        else if(score<75)
+            return 'rgb(89, 145, 78)'
+        else
+            return 'rgb(3, 144, 252)'
     }
 
     render() {
@@ -40,12 +83,32 @@ class StudentCollege extends React.Component {
 
         const college = this.props.college
 
+        let theme=
+        {
+            math: {
+              symbol: '‍800',
+              color: 'rgb(223, 105, 180)'
+            },
+            EBRW: {
+              symbol: '800',
+              color: 'rgb(153, 105, 180)'
+            },
+            ACT: {
+              symbol: '36',
+              color: 'rgb(23, 105, 180)'
+            },
+            rec: {
+              symbol: college.reccomendScore,
+              color: this.getRecColor(college.reccomendScore)
+            }
+        }
+
         return (
            <div className='collegeCard'>
-                <div class="collegeBox0">
-                    <div class="collegeTitle"> {college.name} </div> 
-                    <a class='dropdown-trigger btn' href='#' data-target='dropdown1'> {this.getStatus()} </a>
-                    <ul id='dropdown1' class='dropdown-content'>
+                <div class="collegeBoxTitleAndStatus">
+                    <div class="collegeTitle"> {this.getName()} </div> 
+                    <a class='dropdown-trigger btn' href='#' data-target={'dropdown' + college.key}> {this.getStatus()} </a>
+                    <ul id={'dropdown' + college.key} class='dropdown-content'>
                         <li><a href="#!" onClick={this.changeStatus.bind(this.self, "pending")}>pending</a></li>
                         <li class="divider" tabindex="-1"></li>
                         <li><a href="#!" onClick={this.changeStatus.bind(this.self, "accepted")}>accepted</a></li>
@@ -59,23 +122,36 @@ class StudentCollege extends React.Component {
                         <li><a href="#!" onClick={this.changeStatus.bind(this.self, "withdrawn")}>withdrawn</a></li>
                     </ul>
                 </div>
-                <div class="collegeLocation"> {college.location + " | Rank: " + college.ranking} </div> 
-                <div class="collegeType"> {college.type + " | " + college.admission_rate + "% Acceptance Rate | "  +  college.completion_rate + "% Completion Rate"} </div> 
-                <div class="collegeBox1">
+                <div class="collegeLocation"> {college.location} </div> 
+                <div class="collegeType"> {college.type + " | " + college.admission_rate + "% Acceptance Rate | "  +  college.completion_rate + "% Completion Rate | Rank: " + college.ranking} </div> 
+                <div class="collegeBoxSizeAndMath">
                     <div class="collegeSize1">{"Size:"}</div> 
                     <div class="collegeMath1">{"Average SAT Math: "}<span class="collegeMath2">{college.avg_SAT_Math}</span></div>
                 </div>
-                <div class="collegeSize2"> {college.size} </div> 
-                <div class="collegeBox2">
+                <div class="collegeBoxSizeAndBar">
+                    <div class="collegeSize2"> {college.size}  </div> 
+                    <span class="collegeText">0</span>
+                    <Progress class="mathBar" percent={this.getPercent("math", college.avg_SAT_Math)} status="math" theme={theme} />
+                </div>
+                <div class="collegeBoxCostAndEnglish">
                     <div class="collegeCost1"> {"Avg Price:"} </div> 
                     <div class="collegeEnglish1">{"Average SAT EBRW: "}<span class="collegeEnglish2">{college.avg_EBRW}</span></div>
                 </div>
-                <div class="collegeCost2"> {'$' + college.cost} </div> 
-                <div class="collegeBox3">
+                <div class="collegeBoxCostAndBar">
+                    <div class="collegeCost2"> {'$' + college.cost} </div> 
+                    <span class="collegeText">0</span>
+                    <Progress class="ebrwBar" percent={this.getPercent("ebrw", college.avg_EBRW)} status="EBRW" theme={theme} />
+                </div>
+                <div class="collegeBoxDebtAndACT">
                     <div class="collegeDebt1"> {"Avg Debt:"} </div> 
                     <div class="collegeACT1">{"Average ACT: "}<span class="collegeACT2">{college.avg_ACT}</span></div>
                 </div>
-                <div class="collegeDebt2"> {'$' + college.debt} </div> 
+                <div class="collegeBoxDebtAndBar">
+                    <div class="collegeDebt2"> {'$' + college.debt} </div> 
+                    <span class="collegeText">0</span>
+                    <Progress class="ACTBar" percent={this.getPercent("act", college.avg_ACT)} status="ACT" theme={theme} />
+                </div>
+                <Progress type="circle" width={120} symbolClassName="reccomendBar" percent={this.getPercent("rec", college.reccomendScore)} status="rec" theme={theme} />
            </div>
         );
     }
