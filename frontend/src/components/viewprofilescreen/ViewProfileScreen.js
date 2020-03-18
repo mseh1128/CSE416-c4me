61e5d0dbdfef0152e470a0631ef59d310da0c77e
unknown <joseph.mignone@stonybrook.edu>
2020-03-18 16:45:05 -0400

    added more to the profile screen

--- frontend/src/components/viewprofilescreen/ViewProfileScreen.js
+++ frontend/src/components/viewprofilescreen/ViewProfileScreen.js
@@ -15,7 +15,7 @@ import data from '../test/TestStudentData.json'
 export class ViewProfileScreen extends Component {
     state = 
     {
-
+        disabled: true
     }
 
     render() {
@@ -40,7 +40,64 @@ export class ViewProfileScreen extends Component {
                     </div>
                     <div id="profileList">
                         <div id="generalInfoList">
-                            <span class="profileHeader">General</span>
+                            <div id="generalHSHeader">
+                                <span class="profileHeader">General Information</span>
+                                <span class="profileHeader">General Education</span>
+                            </div>
+                            <div >
+                                <span class="profileText">User ID:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "60px"}} disabled={this.state.disabled} value={student.userID}></input>
+                                <span class="profileText" style={{left: "110px"}}>HS Name:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "100px"}} disabled={this.state.disabled} value={student.high_school_name}></input>
+                            </div>
+                            <div>
+                                <span class="profileText">Name:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "76px"}} disabled={this.state.disabled} value={student.name}></input>
+                                <span class="profileText" style={{left: "125px"}}>HS City:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "127px"}} disabled={this.state.disabled} value={student.high_school_city}></input>
+                            </div>
+                            <div>
+                                <span class="profileText">Residence State:</span>
+                                <input type="textfield" class="profilePrompt" disabled={this.state.disabled} value={student.residence_state}></input>
+                                <span class="profileText" style={{left: "49px"}}>HS State:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "46px"}} disabled={this.state.disabled} value={student.high_school_state}></input>
+                            </div>
+                            <div>
+                                <span class="profileText">Desired major:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "11px"}} disabled={this.state.disabled} value={student.major_1}></input>
+                                <span class="profileText" style={{left: "58px"}}>GPA:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "87px"}} disabled={this.state.disabled} value={student.GPA}></input>
+                            </div>
+                            <div>
+                                <span class="profileText">Second major:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "14px"}} disabled={this.state.disabled} value={student.major_2}></input>
+                                <span class="profileText" style={{left: "61px"}}>Class:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "86px"}} disabled={this.state.disabled} value={student.college_class}></input>
+                            </div>
+                        </div>
+                        <div id="generalInfoList">
+                            <div id="generalHSHeader">
+                                <span class="profileHeader">General Education</span>
+                                <span class="profileHeader">Highschool General Information</span>
+                            </div>
+                            <div >
+                                <span class="profileText">GPA:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "84px"}} disabled={this.state.disabled} value={student.GPA}></input>
+                                <span class="profileText" style={{left: "110px"}}>HS Name:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "100px"}} disabled={this.state.disabled} value={student.high_school_name}></input>
+                            </div>
+                            <div>
+                                <span class="profileText">Name:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "76px"}} disabled={this.state.disabled} value={student.name}></input>
+                                <span class="profileText" style={{left: "125px"}}>HS City:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "127px"}} disabled={this.state.disabled} value={student.high_school_city}></input>
+                            </div>
+                            <div>
+                                <span class="profileText">Residence State:</span>
+                                <input type="textfield" class="profilePrompt" disabled={this.state.disabled} value={student.residence_state}></input>
+                                <span class="profileText" style={{left: "49px"}}>HS State:</span>
+                                <input type="textfield" class="profilePrompt" style={{left: "46px"}} disabled={this.state.disabled} value={student.high_school_state}></input>
+                            </div>
                         </div>
                     </div>
                 </div>
