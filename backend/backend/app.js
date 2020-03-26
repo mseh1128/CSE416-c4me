2413b21e27e10cde52a2cf628fc795e61d9a86b4
Manav Sehgal <manavsehgal1128@gmail.com>
2020-03-26 17:45:39 -0400

    Remove extraneous routes

--- backend/backend/app.js
+++ backend/backend/app.js
@@ -1,61 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-//mysql is because it is what I am connecting to.
-//bodyParser will allow me to accept body texts????
-/
-var mysql = require('mysql');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "gas pedal",
-  database: "college_recommender"
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'gas pedal',
+  database: 'college_recommender'
 });
 
-connection.connect(function(err) {(err) ? console.log("You failed to connect because " + err) : console.log("A connection has been established.")});
+connection.connect(err => {
+  err
+    ? console.log('You failed to connect because ' + err)
+    : console.log('A connection has been established.');
+});
 
 require('./routes/student-functions.js')(app, connection);
 require('./routes/student-acceptance-claims.js')(app, connection);
 require('./routes/user-validation.js')(app, connection);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  res.status(404).send('This page does not exist!');
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send('An error has occurred!');
 });
 
 module.exports = app;
