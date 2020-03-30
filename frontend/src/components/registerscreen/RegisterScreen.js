27df36b5207220a5cb72e147d3c17a69ec7c65d0
unknown <joseph.mignone@stonybrook.edu>
2020-03-30 13:10:30 -0400

    added name to register screen

--- frontend/src/components/registerscreen/RegisterScreen.js
+++ frontend/src/components/registerscreen/RegisterScreen.js
@@ -18,44 +18,38 @@ const nonRepeatingPassword = ' Your password does not match up ';
 const usedIDAndPassword = ' That username and password combo has been used ';
 
 export class RegisterScreen extends Component {
-  state = {
-    userID: '',
-    userIDCheck: '',
-    password: '',
-    passwordCheck: '',
-    hidden: true
-  };
+	state = {
+		userID: '',
+		name: '',
+		password: '',
+		passwordCheck: '',
+		hidden: true
+	};
 
-  checkCredentials = async () => {
-    let password = this.state.password;
-    let username = this.state.userID;
-    let passwordCheck = this.state.passwordCheck;
-    let usernameCheck = this.state.userIDCheck;
+	checkCredentials = async () => {
+		let password = this.state.password;
+		let username = this.state.userID;
+		let name = this.state.name;
+		let passwordCheck = this.state.passwordCheck;
 
-    if (
-      username.length === 0 ||
-      password.length === 0 ||
-      usernameCheck.length === 0 ||
-      passwordCheck.length === 0
-    ) {
-      this.setState({ hidden: false });
-      document.getElementById('errorText').textContent = nothingWasEntered;
-      return;
-    }
+		if (
+			username.length === 0 ||
+			password.length === 0 ||
+			name.length === 0 ||
+			passwordCheck.length === 0
+		) {
+			this.setState({ hidden: false });
+			document.getElementById('errorText').textContent = nothingWasEntered;
+			return;
+		}
 
-    if (username !== usernameCheck) {
-      this.setState({ hidden: false });
-      document.getElementById('errorText').textContent = nonRepeatingUsername;
-      return;
-    }
+		if (password !== passwordCheck) {
+			this.setState({ hidden: false });
+			document.getElementById('errorText').textContent = nonRepeatingPassword;
+			return;
+		}
 
-    if (password !== passwordCheck) {
-      this.setState({ hidden: false });
-      document.getElementById('errorText').textContent = nonRepeatingPassword;
-      return;
-    }
-
-    /*
+		/*
         let supposedUser = await fetch("http://localhost:5201/addANewStudent?username=" + username + "&password=" + password + "&name=samuel")
         let supposedUserAnswer = await supposedUser.json()
         if (supposedUser.status !== 200 || supposedUserAnswer.length !== 0 ){
@@ -65,112 +59,112 @@ export class RegisterScreen extends Component {
         }
         */
 
-    this.props.history.push('/');
-  };
+		this.props.history.push('/');
+	};
 
-  goLogin = () => {
-    this.props.history.push('/');
-  };
+	goLogin = () => {
+		this.props.history.push('/');
+	};
 
-  goHome = () => {
-    this.props.history.push('/home');
-  };
+	goHome = () => {
+		this.props.history.push('/home');
+	};
 
-  handleChange = e => {
-    const { target } = e;
+	handleChange = e => {
+		const { target } = e;
 
-    this.setState(state => ({
-      ...state,
-      [target.id]: target.value
-    }));
-  };
+		this.setState(state => ({
+			...state,
+			[target.id]: target.value
+		}));
+	};
 
-  handleChange = e => {
-    const { target } = e;
+	handleChange = e => {
+		const { target } = e;
 
-    this.setState(state => ({
-      ...state,
-      [target.id]: target.value
-    }));
-  };
+		this.setState(state => ({
+			...state,
+			[target.id]: target.value
+		}));
+	};
 
-  render() {
-    return (
-      <div className="register_screen_container">
-        <div className="registerContainer">
-          <div></div>
-          <span className="loginText"> Register </span>
-          <div id="userID" className="input-field">
-            <label htmlFor="userID" style={{ left: '20px' }}>
-              UserID
-            </label>
-            <input
-              className="active"
-              type="email"
-              name="userID"
-              id="userID"
-              onChange={this.handleChange}
-            />
-          </div>
-          <div id="userIDCheck" className="input-field">
-            <label htmlFor="userIDCheck" style={{ left: '20px' }}>
-              Enter UserID again
-            </label>
-            <input
-              className="active"
-              type="email"
-              name="userIDCheck"
-              id="userIDCheck"
-              onChange={this.handleChange}
-            />
-          </div>
-          <div id="password" className="input-field">
-            <label htmlFor="password" style={{ left: '20px' }}>
-              Password
-            </label>
-            <input
-              className="active"
-              type="password"
-              name="password"
-              id="password"
-              onChange={this.handleChange}
-            />
-          </div>
-          <div id="passwordCheck" className="input-field">
-            <label htmlFor="password" style={{ left: '20px' }}>
-              Enter Password again
-            </label>
-            <input
-              className="active"
-              type="password"
-              name="passwordCheck"
-              id="passwordCheck"
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="loginButtons">
-            <button id="login" onClick={this.checkCredentials}>
-              {' '}
-              submit{' '}
-            </button>
-            <button id="register" onClick={this.goLogin}>
-              {' '}
-              cancel{' '}
-            </button>
-          </div>
-          <span className="errorText" hidden={this.state.hidden}>
-            {' '}
-            email or password does not match{' '}
-          </span>
-        </div>
-        <div className="banner">
-          C4Me
-          <br />
-          Application Assistant
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='register_screen_container'>
+				<div className='registerContainer'>
+					<div></div>
+					<span className='loginText'> Register </span>
+					<div id='userID' className='input-field'>
+						<label htmlFor='userID' style={{ left: '20px' }}>
+							UserID
+						</label>
+						<input
+							className='active'
+							type='email'
+							name='userID'
+							id='userID'
+							onChange={this.handleChange}
+						/>
+					</div>
+					<div id='name' className='input-field'>
+						<label htmlFor='name' style={{ left: '20px' }}>
+							Name
+						</label>
+						<input
+							className='active'
+							type='email'
+							name='name'
+							id='name'
+							onChange={this.handleChange}
+						/>
+					</div>
+					<div id='password' className='input-field'>
+						<label htmlFor='password' style={{ left: '20px' }}>
+							Password
+						</label>
+						<input
+							className='active'
+							type='password'
+							name='password'
+							id='password'
+							onChange={this.handleChange}
+						/>
+					</div>
+					<div id='passwordCheck' className='input-field'>
+						<label htmlFor='password' style={{ left: '20px' }}>
+							Enter Password again
+						</label>
+						<input
+							className='active'
+							type='password'
+							name='passwordCheck'
+							id='passwordCheck'
+							onChange={this.handleChange}
+						/>
+					</div>
+					<div className='loginButtons'>
+						<button id='login' onClick={this.checkCredentials}>
+							{' '}
+							submit{' '}
+						</button>
+						<button id='register' onClick={this.goLogin}>
+							{' '}
+							cancel{' '}
+						</button>
+					</div>
+					<span className='errorText' hidden={this.state.hidden}>
+						{' '}
+						email or password does not match{' '}
+					</span>
+				</div>
+				<div className='banner'>
+					C4Me
+					<br />
+					Application Assistant
+				</div>
+			</div>
+		);
+	}
 }
 
 export default RegisterScreen;
