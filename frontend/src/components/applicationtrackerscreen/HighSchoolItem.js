80b6b962f5474795dab9ba304af5f481e7a46a87
unknown <joseph.mignone@stonybrook.edu>
2020-04-30 14:10:49 -0400

    trying to dynamic link

--- frontend/src/components/applicationtrackerscreen/HighSchoolItem.js
+++ frontend/src/components/applicationtrackerscreen/HighSchoolItem.js
@@ -5,7 +5,13 @@ import M from 'materialize-css';
 
 import { Link } from 'react-router-dom';
 
+import axios from 'axios';
+
 class HighSchoolItem extends React.Component {
+	state = {
+		highschools: '',
+	};
+
 	getName = () => {
 		let name = this.props.highschool.name;
 		if (name === null || name === undefined) {
@@ -17,29 +23,59 @@ class HighSchoolItem extends React.Component {
 		} else return name;
 	};
 
+	componentDidMount = async () => {
+		try {
+			const potentialHighSchools = await axios.get('/getHighSchoolFromName', {
+				params: {
+					highSchoolName: this.props.highschool.name,
+				},
+			});
+			console.log(potentialHighSchools.data);
+			this.setState({ highschools: potentialHighSchools.data });
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	getLink = async () => {
+		console.log(this.state.highschools);
+		console.log(this.state.highschools.length);
+		console.log(this.props.college);
+		let link = {};
+
+		if (this.state.highschools.length === 0) {
+		} else if (this.state.highschools.length === 1) {
+			link = {
+				pathname:
+					'/applicationTracker/' +
+					this.props.college.collegeName +
+					'/highSchools/' +
+					this.props.highschool.name,
+				state: {
+					college: this.props.college,
+				},
+			};
+		} else if (this.state.highschools.length > 1) {
+			link = {
+				pathname: '/applicationTracker/' + this.props.college.collegeName + '/possibleHighSchools',
+				state: {
+					college: this.props.college,
+					highschoolName: this.props.highschool.name,
+				},
+			};
+			console.log(link);
+		}
+		return { link };
+	};
+
 	render() {
 		const highschool = this.props.highschool;
 
 		return (
 			<div className='trackerHighschoolItem'>
 				<span> {this.getName()} </span>
-				<Link
-					to={{
-						pathname:
-							'/applicationTracker/' + this.props.college.collegeName + '/possibleHighSchools',
-						state: {
-							college: this.props.college,
-							highschoolName: highschool.name,
-						},
-					}}
-				>
-					<button
-						className='similarHSBtn'
-						//onClick={this.props.goSimilarHighSchools.bind(this.self, highschool.name)}
-					>
-						{' '}
-						Similar{' '}
-					</button>
+				<Link to={this.getLink()}>
+					<button className='similarHSBtn'> Similar </button>
 				</Link>
 
 				<button
